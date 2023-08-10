703ef7a305547b5e9970fda646129eea445ecd3e
pleblira <91032628+pleblira@users.noreply.github.com>
2023-08-10 11:01:07 -0400

    bug fix

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def bot58k():
 
     text_on_images(f"{price_58k:.4f}", "58k_bot.png")
     url_of_note_on_snort_social = post_note(upload_to_voidcat_and_return_url("58k_bot_with_text.png", "png"))
-    tweet_with_apiv2("58k update"+"\n\nNOSTR: "+url_of_note_on_snort_social", "58k_bot_with_text.png")
+    tweet_with_apiv2("58k update"+"\n\nNOSTR: "+url_of_note_on_snort_social, "58k_bot_with_text.png")
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
